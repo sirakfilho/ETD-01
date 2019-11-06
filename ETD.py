ef552f96e04b35d6eb63bcfe6b86c6390ed7064b
Sirak Filho <51516586+sirakfilho@users.noreply.github.com>
2019-11-06 20:49:28 -0300

    Add files via upload

---
+++ ETD.py
@@ -0,0 +1,17 @@
+import Etdclass
+
+def main():
+    a = Etdclass()
+    b =
+    cont = 0
+    while(cont == 0):
+
+        #Escolher Academico ou Administrativo
+
+        opcao1 = input("Digite 'a' para Academico , 'b' para Administrativo ou '0' , para sair do programa: ")
+        print("\n====================================================================\n")
+        if(opcao1 == "b" ):
+            cont2 = 0
+            while(cont2 == 0):
+
+main()
---
+++ Etdclass.py
@@ -0,0 +1,15 @@
+class Etdclass:
+    def __init__(self):
+        # variaveis do Academico
+
+        self.bancoAcademico = []
+        self.nomeAcademico = ""
+        self.tituloAcademico = ""
+        self.descricaoAcademico = ""
+        self.numParticipantesAcademico = ""
+        self.chaveAcademico = ""
+        # variaveis de Reuniao
+
+        self.agendarReuniao = []
+        self.registrarReuniao = ""
+        self.arquivodeReuniao  = ""
